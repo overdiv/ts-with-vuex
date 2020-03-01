bbd20b96715669ab410063c68ceb7d97abc4204c
overdiv <stancode203@gmail.com>
2020-03-02 02:16:28 +0900

    feat: store class 선언 방식 적용

--- ts-vuex/src/store/moduleA.store.ts
+++ ts-vuex/src/store/moduleA.store.ts
@@ -1,3 +1,4 @@
+// 객체 모듈 방식
 import { Module } from 'vuex';
 import { RootState } from './store';
 
@@ -27,3 +28,29 @@ const module: Module<moduleA, RootState> = {
 }
 
 export default module;
+
+
+// 클래스형 모듈 방식
+// import { Module, VuexModule, Mutation, Action } from 'vuex-module-decorators';
+
+// @Module({
+//   namespaced: true,
+//   name: 'moduleA',
+// })
+// export default class ModuleA extends VuexModule {
+//   public data: string = 'moduleA';
+
+//   @Mutation
+//   public setData(data: string) {
+//     this.data = data;
+//   }
+
+//   @Action
+//   public editData(data: string) {
+//     this.context.commit('setData', data);
+//   }
+
+//   get moduleAdata() {
+//     return this.data;
+//   }
+// }
